401ce6869c3bf2d368e055865dcf789b8114d595
unknown <benyahdou.moh29@hotmail.com>
2024-02-21 11:34:20 +0100

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,17 @@ import { SearchBar } from "@/components/SearchBar";
 import { CustomFilter } from "@/components/CustomFilter";
 import { fetchCars } from "@/utils";
 import {CarCard} from "@/components/CarCard"
-export default async function Home() {
-  const allCars = await fetchCars();
+
+export default async function Home({searchParams}) {
+  
+  const allCars = await fetchCars({
+    manufacturer: searchParams.manufacturer || "",
+    year: searchParams.year || 2022,
+    fuel: searchParams.fuel || "",
+    limit: searchParams.limit || 10,
+    model: searchParams.model || "",
+  });
+
   const isDataEmpty = !Array.isArray(allCars) || allCars.length<1 || !allCars
   
   return (
@@ -20,8 +29,8 @@ export default async function Home() {
         <div className="home__filters">
           <SearchBar/>
           <div className="home__filter-container">
-            <CustomFilter />
-            <CustomFilter />
+            <CustomFilter title="fuel"/>
+            <CustomFilter title="year"/>
           </div>
         </div>
         {!isDataEmpty?(
--- utils/index.ts
+++ utils/index.ts
@@ -1,11 +1,15 @@
-import { CarProps } from "@/types";
+import { CarProps, FilterProps } from "@/types";
 
-export async function fetchCars(){
+export async function fetchCars(filters: FilterProps){
+	const { manufacturer, year, model, limit, fuel } = filters;
 	const headers = {
 		'X-RapidAPI-Key': 'f5f31e2244msh53ffa0d470ad83ep11ff77jsnd216560387c7',
 		'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
 	}
-	const response = await fetch("https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla",{headers:headers});
+	const response = await fetch(`https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`,
+    {
+      headers: headers,
+    });
 	const result= await response.json();
 	return result; 
 }
